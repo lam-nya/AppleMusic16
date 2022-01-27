a2b738ab922e2a79bbde1d6ec4d1320912e8f9eb
Igor Kuzmin <lamnya@gmail.com>
2022-01-27 11:34:49 +0800

    Initial Commit

---
+++ AppleMusic16/AppleMusic16App.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic16App.swift
+//  AppleMusic16
+//
+//  Created by Igor Kuzmin on 27.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
